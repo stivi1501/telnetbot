9f2e5f38f088f400c02a3e11c697f6f44ca76901
stivi1501 <stefan.wrobel@hotmail.com>
2018-05-23 21:53:40 +0200

    MYSQL 2-gie powiązanie logowania telnetem z bazą danych

--- src/telnetbot/telnetbot.java
+++ src/telnetbot/telnetbot.java
@@ -18,17 +18,14 @@ for (int i=0;i<maks;i++)
 {
 	//System.out.println(tp.lista.get(i).device+" "+tp.lista.get(i).port+" "+tp.lista.get(i).user+" "+tp.lista.get(i).pass+" "+tp.lista.get(i).lo+" "+tp.lista.get(i).pa);
 }
-int i=0;
-  //  for(int i=0; i<=maks; i++) 
+//int i=0;
+    for(int i=0; i<maks; i++) 
      {
-    	// runners[i] = new telnetbot_thread("192.168.1.5",23, "stivi", "guide","login: ","Password: ");;
     	 runners[i] = new telnetbot_thread(tp.lista.get(i).device,tp.lista.get(i).port, tp.lista.get(i).user,tp.lista.get(i).pass,tp.lista.get(i).lo,tp.lista.get(i).pa);
-    	 
-    	//runners[i] = new telnetbot_thread(tp.lista.get(i).device,tp.lista.get(i).port,tp.lista.get(i).user,tp.lista.get(i).pass,tp.lista.get(i).lo,tp.lista.get(i).pa);;
      }       
-  //  for(int i=0; i<maks; i++) 
+   for(int i=0; i<maks; i++) 
     {threads[i] = new Thread(runners[i]);}
-  //  for(int i=0; i<maks; i++) 
+    for(int i=0; i<maks; i++) 
     {threads[i].start();}
 
     }
